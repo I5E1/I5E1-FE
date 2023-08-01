d11e3aba6372138c855d337fedc06f1f91599a20
azure0929 <chary8028@naver.com>
2023-08-01 21:46:58 +0900

    Feat: 추가 로그인 페이지 레이아웃 회원가입 부분

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,11 @@
 export default function Login () {
   return (
-    <div className='flex border h-screen'>
+    <div className='flex h-screen font-Noto'>
       <div className='w-[80rem] p-8'>
         <div className='mb-20'>
           <img src='/logo.png' alt='logo' />
         </div>
-        <div className='w-[34rem] mx-auto font-Noto tracking-tighter font-medium text-3xl'>
+        <div className='w-[34rem] mx-auto tracking-tighter font-medium text-3xl'>
           <div className='mb-11'>
             <p className='mb-3'>안녕하세요</p>
             <p>
@@ -47,7 +47,20 @@ export default function Login () {
           </div>
         </div>
       </div>
-      <div className='w-[40rem] rounded-l-[30px] p-8 bg-[#0474E4]'>
+      <div className='w-[40rem] rounded-l-[30px] p-8 bg-[#0474E4] text-white tracking-tighter'>
+        <div className='text-center mt-60 mb-5'>
+          <h3 className='text-3xl mb-12'>환영합니다!</h3>
+          <p className='mb-20'>
+            연차와 당직 관리<br />
+            이제는 <span className='font-Spoqa tracking-normal font-bold text-2xl'>RE:POST</span>와 함께
+          </p>
+          <button 
+            className='w-60 h-16 py-2.5 rounded-[30px] border border-white font-normal text-base text-white hover:bg-[#fff] hover:text-[#0474e4] transition duration-300'
+            type="submit"
+          >
+            회원가입
+          </button>
+        </div>
         <div>
           <img src='/worker.png' alt='worker' />
         </div>

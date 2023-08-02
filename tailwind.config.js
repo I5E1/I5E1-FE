7cf980f540cce3ec12b50177610989b129925cda
Kim JinWoo <kimjinu2540@gmail.com>
2023-08-02 19:41:29 +0900

    twt

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@
 
 module.exports = {
   // 모든 HTML 템플릿, JavaScript 구성 요소 및 Tailwind 클래스 이름이 포함된 기타 소스 파일에 대한 경로
-  content: ['./src/pages/**/*.{js,ts,jsx,tsx,mdx}', './src/pages/*.{js,ts,jsx,tsx,mdx}', './src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
 
   // 색상 팔레트, 문자 배율, 글꼴, 중단점, 테두리 반경 값 등을 정의하는 곳입니다.
   theme: {
@@ -20,28 +25,22 @@ module.exports = {
 
     // 폰트 지정
     fontFamily: {
-      'Noto': ['Noto Sans KR', 'sans-serif'],
-      'Spoqa': ['Spoqa Han Sans Neo', 'sans-serif']
+      Noto: ['Noto Sans KR', 'sans-serif'],
+      Spoqa: ['Spoqa Han Sans Neo', 'sans-serif']
     },
-    extend: {
-      width: {
-        '1280': '80rem',
-        '640': '40rem',
-        '448': '34rem'
-      },
-      borderRadius: {
-        'lg': '30px'
-      },
-      color: {
-        'primary': '#0474e4',
-        'secondary': '#029104',
-        'btn-default': '#d9d9d9',
-        'text-secondary': '#8e8e8e'
-      }
-    }
+
+    colors: {
+      primary: '#0474e4',
+      secondary: '#029104',
+      'btn-default': '#d9d9d9',
+      'text-secondary': '#8e8e8e'
+    },
 
     //전역 간격 및 크기 조정 척도를 사용자 정의
-    // spacing: {}
+    width: {
+      main: '1280px',
+      sub: '640px'
+    }
   },
   plugins: []
 }

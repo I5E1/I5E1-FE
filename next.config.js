d2a5dae83f1102cf7074b9bda68606119c5bc3e7
Kim JinWoo <kimjinu2540@gmail.com>
2023-08-10 20:47:49 +0900

    배포 설정

--- next.config.js
+++ next.config.js
@@ -9,10 +9,6 @@ const nextConfig = {
       {
         source: '/:path*',
         destination: 'http://3.34.59.206:8080/:path*'
-      },
-      {
-        source: '/api',
-        destination: 'http://localhost:3000'
       }
     ]
   }
